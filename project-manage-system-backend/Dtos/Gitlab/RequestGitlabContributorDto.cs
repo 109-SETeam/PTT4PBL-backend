6a9dfa6ae54bc702c6f13e44786e3e8b33213dfe
zxjte9411 <a5852241@gmail.com>
2021-03-18 15:00:25 +0800

    implement RequestGitlabContributorDto

---
+++ project-manage-system-backend/Dtos/Gitlab/RequestGitlabContributorDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Dtos
+{
+    public class Stats
+    {
+        public int additions { get; set; }
+        public int deletions { get; set;}
+        public int total { get; set;}
+    }
+    public class RequestGitlabContributorDto
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public int commits { get; set; }
+        public Stats stats{get; set;}
+    }
+}
