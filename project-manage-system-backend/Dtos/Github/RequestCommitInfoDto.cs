66ded5e6ef672dc728d7bda84f15c805ef17db96
zxjte9411 <a5852241@gmail.com>
2021-04-22 17:38:16 +0800

    補註解

--- project-manage-system-backend/Dtos/Github/RequestCommitInfoDto.cs
+++ project-manage-system-backend/Dtos/Github/RequestCommitInfoDto.cs
@@ -4,22 +4,25 @@ namespace project_manage_system_backend.Dtos
 {
     public class WeekTotalData
     {
+        // First day of the week
         public string Week { get; set; }
-
+        // Total commits of the week
         public int Total { get; set; }
     }
 
     public class DayCommit
     {
+        // Day of the week
         public string Day { get; set; }
-
+        // Commit count of the day
         public int Commit { get; set; }
     }
 
     public class DayOfWeekData
     {
+        // First day of the week
         public string Week { get; set; }
-
+        
         public List<DayCommit> DetailDatas { get; set; }
     }
 
