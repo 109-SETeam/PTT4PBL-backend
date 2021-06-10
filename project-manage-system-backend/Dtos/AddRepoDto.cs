41b65eb3f89b2ef106bd09c1752537bf20fc5510
zxjte9411 <a5852241@gmail.com>
2021-06-10 16:17:55 +0800

    移除不需要的屬性

--- project-manage-system-backend/Dtos/AddRepoDto.cs
+++ project-manage-system-backend/Dtos/AddRepoDto.cs
@@ -7,22 +7,5 @@
 
         public string url { get; set; }
 
-        public bool isSonarqube { get; set; }
-
-        /// <summary>
-        /// foramt: "帳號:密碼"
-        /// encryption:base64
-        /// </summary>
-        public string accountColonPassword { get; set; }
-
-        /// <summary>
-        /// Sonarqube網址
-        /// </summary>
-        public string sonarqubeUrl { get; set; }
-
-        /// <summary>
-        /// sonarqube projectkey
-        /// </summary>
-        public string projectKey { get; set; }
     }
 }
